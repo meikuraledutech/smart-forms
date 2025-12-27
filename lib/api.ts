6decd7f712d2c6161feaa813abd1464eed3a8d5b
Meikural official <meikuraledutech@gmail.com>
2025-12-27 19:43:10 +0530

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,14 +1,14 @@
 // lib/api.ts
 
 // const DEV_API_URL = "https://7ca0cf1a82a3.ngrok-free.app"
-const DEV_API_URL = "http://localhost:3030"
+const DEV_API_URL = "https://267f10340f55.ngrok-free.app";
 function normalize(url: string) {
-    return url.replace(/\/+$/, "")
+  return url.replace(/\/+$/, "");
 }
 
 const base =
-    process.env.NODE_ENV === "development"
-        ? DEV_API_URL
-        : process.env.NEXT_PUBLIC_API_URL || ""
+  process.env.NODE_ENV === "development"
+    ? DEV_API_URL
+    : process.env.NEXT_PUBLIC_API_URL || "";
 
-export const API_BASE_URL = normalize(base)
+export const API_BASE_URL = normalize(base);
